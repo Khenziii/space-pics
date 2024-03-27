a59de12b10918103d45b3180e7488ad71c6edebe
Khenziii <khenzii.was.taken@gmail.com>
2024-03-27 18:40:17 +0100

    feat: publish media to twitter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from config import Config
 from nasa import NasaAPI
 import tweepy
 from datetime import date
+import os
 
 configManager = Config()
 secrets = configManager.secrets
@@ -10,13 +11,24 @@ nasa_client = NasaAPI(
     api_key=secrets["NASA_API_DEMO_KEY"]
 )
 
-# twitter_client = tweepy.Client(
-#     consumer_key=secrets["CONSUMER_KEY"],
-#     consumer_secret=secrets["CONSUMER_SECRET_KEY"],
-#     access_token=secrets["ACCESS_TOKEN"],
-#     access_token_secret=secrets["ACCESS_TOKEN_SECRET"]
-# )
-# twitter_client.create_tweet(text="Hello, World!")
+twitter_auth_dict = {
+    "consumer_key": secrets["CONSUMER_KEY"],
+    "consumer_secret": secrets["CONSUMER_SECRET_KEY"],
+    "access_token": secrets["ACCESS_TOKEN"],
+    "access_token_secret": secrets["ACCESS_TOKEN_SECRET"]
+}
+twitter_client = tweepy.Client(**twitter_auth_dict)
+twitter_auth = tweepy.OAuth1UserHandler(**twitter_auth_dict)
+twitter_api = tweepy.API(auth=twitter_auth)
 
 from_when: date = date.today()
-print(nasa_client.get_pictures(from_when, True))
+thumbs: bool = True
+download_image: bool = True
+data = nasa_client.get_pictures(from_when, thumbs, download_image)
+
+image_description = data["title"]
+media = twitter_api.media_upload("image.png")
+os.remove("image.png")
+
+tweet_content = f"{image_description}\n\n{from_when.strftime('%d/%m/%Y')}"
+twitter_client.create_tweet(text=tweet_content, media_ids=[media.media_id])
--- nasa.py
+++ nasa.py
@@ -6,7 +6,7 @@ class NasaAPI:
     def __init__(self, api_key: str):
         self.key = api_key
 
-    def get_pictures(self, from_when: date, thumbs: bool):
+    def get_pictures(self, from_when: date, thumbs: bool, download_image: bool):
         request_url = f"https://api.nasa.gov/planetary/apod"
         request_url += f"?api_key={self.key}"
         request_url += f"&thumbs={thumbs}"
@@ -15,6 +15,13 @@ class NasaAPI:
         try:
             response = requests.get(request_url)
             response.raise_for_status()
-            return response.json()
+            json = response.json()
+
+            if download_image:
+                image = requests.get(json["url"])
+                with open("image.png", "wb") as f:
+                    f.write(image.content)
+
+            return json
         except Exception as e:
             print(f"Something went wrong while fetching data from NASA's API! Error: {e}")
